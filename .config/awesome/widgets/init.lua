814b2d34bcf2b0eb08f6e876ea5599f7f37a238c
Adel Sehic <adelsehic95@gmail.com>
2024-09-19 18:20:57 +0200

    - Alacritty, awesome, nvim

---
+++ .config/awesome/widgets/init.lua
@@ -0,0 +1,20 @@
+--[[ Docstring
+
+    My widgets created for Awesome WM
+
+    (c) Copyright 2023, Abiel Mendes (<https://github.com/paodelonga>)
+    https://github.com/paodelonga/awesome-widgets
+
+--]]
+
+-- To activate a Widget move the line out of the comment block.
+return {
+    --[[
+    weather = require("widgets.weather")
+    apt_updates = require("widgets.apt_updates")
+    --]]
+    spotify_bar = require("widgets.spotify_bar"),
+    -- pavu = require("widgets.awesome-pulseaudio-widget")
+}
+
+--[>D]
